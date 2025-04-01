09b3db22b2ad159cb789a2fb8c3ae73e87066563
hexstr <arraytoobig@gmail.com>
2025-04-01 10:56:51 +0800

    First init.

---
+++ BlueArchive_AssetModifyTool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueArchive_AssetModifyTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
